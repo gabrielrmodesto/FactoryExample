12492e8cc3dadf99790b2f0609818d6793f72117
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2022-02-07 23:48:01 -0300

    Client code using methodfactory

--- index.ts
+++ index.ts
@@ -27,6 +27,14 @@ class PaypalMethod implements PaymentMethod {
   }
 }
 
+//factory do paypal
+
+class PaypalMethodFactory extends PaymentMethodFactory {
+  create(): PaymentMethod {
+    return new PaypalMethod();
+  }
+}
+
 class PagseguroMethod implements PaymentMethod {
   tax: number = 0.02;
   createPayment(orderId: string): boolean {
@@ -40,3 +48,26 @@ class PagseguroMethod implements PaymentMethod {
     return true;
   }
 }
+
+class PagseguroMethodFactory extends PaymentMethodFactory {
+  create(): PaymentMethod {
+    return new PagseguroMethod();
+  }
+}
+
+function clientCode() {
+  let paymentMethod: PaymentMethod;
+
+  //pagamento via paypal
+  const paypalMethodFactory = new PaypalMethodFactory();
+  paymentMethod = paypalMethodFactory.create();
+  paymentMethod.createPayment("Order01");
+
+  //pagamento via pagseguro
+  const pagseguroMethodFactory = new PagseguroMethodFactory();
+  paymentMethod = pagseguroMethodFactory.create();
+  paymentMethod.createPayment("Order02");
+  paymentMethod.refoundPayment("Order02");
+}
+
+clientCode();
